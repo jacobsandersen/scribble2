35396563d02eb1c81d5527a5ae0a2a40ff8dcaef
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2026-05-08 21:22:24 +0800

    set env key separator

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
   debug!("loading configuration...");
   let config: ScribbleConfig = Config::builder()
     .add_source(File::with_name("config").required(false))
-    .add_source(Environment::default())
+    .add_source(Environment::default().separator("__"))
     .build()?
     .try_deserialize()?;
 
